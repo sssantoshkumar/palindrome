dc859064db727e423762eed710429308fb21e774
santoshkumar <sssantoshkumar368@gmail.com>
2021-02-18 15:13:10 +0530

    Create assign_28.4.py
    hujk

---
+++ assign_28.4.py
@@ -0,0 +1,6 @@
+string='welcome to palle technologies'
+i=0
+while i<=len(string)-1:
+    if (string[i] == 'a' or string[i] == 'e' or string[i] == 'i' or string[i] == 'o' or string [i] =='u'):
+        print(string[i])
+    i=i+1
